625d9a8c098208847ca4599cf586eb93f6591933
zhanghongda <zhanghongda@atwasoft.net>
2020-07-09 11:25:59 +0800

    'todolist实例练习'

---
+++ src/pages/home/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div className='home'>
+        <h1>在react中使用mobx</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
